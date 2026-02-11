68339b7d765478fcae698653766549eb914e9149
Neil <tenet113219@gmail.com>
2026-02-11 23:51:46 +0800

    list

---
+++ Day2/practice1.py
@@ -0,0 +1,23 @@
+
+a=[10,20,30]
+print(a)
+
+b=list(range(5))
+print(b)
+
+cons=[]
+for i in range(5):
+    cons.append(i)
+print(cons)
+
+#slicing
+cons_slice=cons[1:4]
+print("slice 1:4:",cons_slice)
+
+#反轉
+print("reverse: ",a[::-1])
+
+
+
+
+
