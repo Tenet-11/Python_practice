3830cf2e41c6fdc00d8f5c33060eec05750d3f65
Neil <tenet113219@gmail.com>
2026-02-11 21:30:40 +0800

    pr1

--- Day1/practice1.py
+++ Day1/practice1.py
@@ -3,3 +3,25 @@ print(type(a))
 
 a = "hello"
 print(type(a))
+
+b=10
+c=b
+b=20
+print(b)
+
+ss=[1,2,3]
+e=ss
+ss.append(4)
+print(e)
+
+# 練習 3（理解記憶體）
+f="hello"
+print(id(f))
+f+=" world"
+print(id(f))
+
+# ID變了，字串是 immutable
+
+#   id 變了
+#    = 記憶體位置變了
+#    = 是新物件
